a66def97d9c43066c296de19b30408a3f028ae19
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2019-12-30 22:21:07 +0100

    Moved dev setup code to test suite

--- tests/testthat/setup-metadata.R
+++ tests/testthat/setup-metadata.R
@@ -1,6 +1,56 @@
 library(emuR)
 library(dplyr)
 
+
+
+create_ae_db <- function(){
+  demodir <- file.path(tempdir(),"emuR_demoData")
+
+  if(!dir.exists(demodir)){
+    create_emuRdemoData()
+  }
+  db <- load_emuDB(file.path(demodir,"ae_emuDB"))
+  return(db)
+}
+
+
+
+
+make_dummy_metafiles <- function(db){
+  # We need two copies of the session, which in R requires som manual intervention
+  dirs <- list.dirs(file.path(db$basePath,"0000_ses"))
+  dirs <- gsub("0000_ses","0001_ses",dirs)
+  for(currDir in dirs){
+    dir.create(currDir,showWarnings = FALSE,recursive = TRUE)
+  }
+
+  inFiles <- list.files(file.path(db$basePath,"0000_ses"),full.names = TRUE,recursive = TRUE,include.dirs = FALSE)
+  outFiles <- gsub("0000_ses","0001_ses",inFiles)
+  file.copy(from=inFiles,to=outFiles,recursive = FALSE)
+
+  sess1 <- file.copy(from=file.path("..","session.meta"),to=file.path(db$basePath,"0000_ses","0000.meta_json"))
+
+  sess2 <- file.copy(from=file.path("..","session_0001.meta"),to=file.path(db$basePath,"0001_ses","0001.meta_json"))
+  outMetaFiles <- list_files(db,"wav") %>%
+    select(absolute_file_path) %>%
+    mutate(absolute_file_path=gsub("wav$",metadata.extension,absolute_file_path)) %>%
+    slice(-2,-9) ## One file per session should be missing so that we may test bundle and session defaults
+
+  res <- file.copy(from=file.path("..","bundle.meta"),to=outMetaFiles[[1]])
+
+  res <- c(sess1,sess2,res)
+  # store database wide default values
+  emuR:::load_DBconfig(db) -> dbCfg
+  dbCfg$metadataDefaults <- jsonlite::read_json(file.path("..","db.meta"),simplifyVector = FALSE) ## Keep the list strucure since this is going directly to json
+  emuR:::store_DBconfig(db,dbConfig = dbCfg)
+
+  return(res)
+}
+
+
+
+
+unlink_emuRDemoDir()
 create_ae_db() -> ae_test
 make_dummy_metafiles(ae_test)
 
--- tests/testthat/teardown-metadata.R
+++ tests/testthat/teardown-metadata.R
@@ -1,2 +1,10 @@
+unlink_emuRDemoDir <- function(){
+  demodir <- file.path(tempdir(),"emuR_demoData")
+  res <- unlink(demodir,recursive = TRUE)
+  binRes <- c(TRUE,FALSE)[res+1]
+  return(binRes)
+}
+
+
 
 unlink_emuRDemoDir()
