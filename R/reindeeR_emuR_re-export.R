53d4f39a72bf0045b88d66098b6d003185100f38
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-03-31 16:07:03 +0200

    Added re-export of EmuR functions

---
+++ R/reindeeR_emuR_re-export.R
@@ -0,0 +1,333 @@
+
+#' Define a label group for an attribute definition (from emuR)
+#' @export
+#' @inheritParams emuR::add_attrDefLabelGroup
+#' @inherit emuR::add_attrDefLabelGroup return references description details sections seealso
+#' @seealso [emuR::add_attrDefLabelGroup]
+add_attrDefLabelGroup = emuR::add_attrDefLabelGroup
+
+#' List label groups defined for an attribute definition (from emuR)
+#' @export
+#' @inheritParams emuR::list_attrDefLabelGroups
+#' @inherit emuR::list_attrDefLabelGroups return references description details sections seealso
+#' @seealso [emuR::list_attrDefLabelGroups]
+list_attrDefLabelGroups = emuR::list_attrDefLabelGroups
+
+#' Remove a label group from an attribute definition (from emuR)
+#' @export
+#' @inheritParams emuR::remove_attrDefLabelGroup
+#' @inherit emuR::remove_attrDefLabelGroup return references description details sections seealso
+#' @seealso [emuR::remove_attrDefLabelGroup]
+remove_attrDefLabelGroup = emuR::remove_attrDefLabelGroup
+
+#' Add a global label group (from emuR)
+#' @export
+#' @inheritParams emuR::add_labelGroup
+#' @inherit emuR::add_labelGroup return references description details sections seealso
+#' @seealso [emuR::add_labelGroup]
+add_labelGroup = emuR::add_labelGroup
+
+#' Remove a global label group (from emuR)
+#' @export
+#' @inheritParams emuR::remove_labelGroup
+#' @inherit emuR::remove_labelGroup return references description details sections seealso
+#' @seealso [emuR::remove_labelGroup]
+remove_labelGroup = emuR::remove_labelGroup
+
+#' List global label groups (from emuR)
+#' @export
+#' @inheritParams emuR::list_labelGroups
+#' @inherit emuR::list_labelGroups return references description details sections seealso
+#' @seealso [emuR::list_labelGroups]
+list_labelGroups = emuR::list_labelGroups
+
+#' Add a level definition (from emuR)
+#' @export
+#' @inheritParams emuR::add_levelDefinition
+#' @inherit emuR::add_levelDefinition return references description details sections seealso
+#' @seealso [emuR::add_levelDefinition]
+add_levelDefinition = emuR::add_levelDefinition
+
+#' Remove a level definition (from emuR)
+#' @export
+#' @inheritParams emuR::remove_levelDefinition
+#' @inherit emuR::remove_levelDefinition return references description details sections seealso
+#' @seealso [emuR::remove_levelDefinition]
+remove_levelDefinition = emuR::remove_levelDefinition
+
+#' List level definitions (from emuR)
+#' @export
+#' @inheritParams emuR::list_levelDefinitions
+#' @inherit emuR::list_levelDefinitions return references description details sections seealso
+#' @seealso [emuR::list_levelDefinitions]
+list_levelDefinitions = emuR::list_levelDefinitions
+
+#' Add a level definition (from emuR)
+#' @export
+#' @inheritParams emuR::add_linkDefinition
+#' @inherit emuR::add_linkDefinition return references description details sections seealso
+#' @seealso [emuR::add_linkDefinition]
+add_linkDefinition = emuR::add_linkDefinition
+
+#' Remove a level definition (from emuR)
+#' @export
+#' @inheritParams emuR::remove_linkDefinition
+#' @inherit emuR::remove_linkDefinition return references description details sections seealso
+#' @seealso [emuR::remove_linkDefinition]
+remove_linkDefinition = emuR::remove_linkDefinition
+
+#' List level definitions (from emuR)
+#' @export
+#' @inheritParams emuR::list_linkDefinitions
+#' @inherit emuR::list_linkDefinitions return references description details sections seealso
+#' @seealso [emuR::list_linkDefinitions]
+list_linkDefinitions = emuR::list_linkDefinitions
+
+#' Define a new attribute for a level (from emuR)
+#' @export
+#' @inheritParams emuR::add_attributeDefinition
+#' @inherit emuR::add_attributeDefinition return references description details sections seealso
+#' @seealso [emuR::add_attributeDefinition]
+add_attributeDefinition = emuR::add_attributeDefinition
+
+#' Remove an attribute from a level (from emuR)
+#' @export
+#' @inheritParams emuR::remove_attributeDefinition
+#' @inherit emuR::remove_attributeDefinition return references description details sections seealso
+#' @seealso [emuR::remove_attributeDefinition]
+remove_attributeDefinition = emuR::remove_attributeDefinition
+
+#' List attributes defined for a level (from emuR)
+#' @export
+#' @inheritParams emuR::list_attributeDefinitions
+#' @inherit emuR::list_attributeDefinitions return references description details sections seealso
+#' @seealso [emuR::list_attributeDefinitions]
+list_attributeDefinitions = emuR::list_attributeDefinitions
+
+#' Define an SSFF track for the emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::add_ssffTrackDefinition
+#' @inherit emuR::add_ssffTrackDefinition return references description details sections seealso
+#' @seealso [emuR::add_ssffTrackDefinition]
+add_ssffTrackDefinition = emuR::add_ssffTrackDefinition
+
+#' Remove an SSFF track from the emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::add_ssffTrackDefinition
+#' @inherit emuR::remove_ssffTrackDefinition return references description details sections seealso
+#' @seealso [emuR::remove_ssffTrackDefinition]
+remove_ssffTrackDefinition = emuR::remove_ssffTrackDefinition
+
+#' List which SSFF tracks are defined for the emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::add_ssffTrackDefinition
+#' @inherit emuR::list_ssffTrackDefinitions return references description details sections seealso
+#' @seealso [emuR::list_ssffTrackDefinitions]
+list_ssffTrackDefinitions = emuR::list_ssffTrackDefinitions
+
+#' Define which labels are legal for an attribute of a level (from emuR)
+#' @export
+#' @inheritParams emuR::set_legalLabels
+#' @inherit emuR::set_legalLabels return references description details sections seealso
+#' @seealso [emuR::set_legalLabels]
+set_legalLabels = emuR::set_legalLabels
+
+
+#' Remove the definition of which labels are legal for an attribute of a level (from emuR)
+#' @export
+#' @inheritParams emuR::remove_legalLabels
+#' @inherit emuR::remove_legalLabels return references description details sections seealso
+#' @seealso [emuR::remove_legalLabels]
+remove_legalLabels = emuR::remove_legalLabels
+
+#' Get which labels are legal for an attribute of a level (from emuR)
+#' @export
+#' @inheritParams emuR::get_legalLabels
+#' @inherit emuR::get_legalLabels return references description details sections seealso
+#' @seealso [emuR::get_legalLabels]
+get_legalLabels = emuR::get_legalLabels
+
+
+
+#' Add a new perspective to an emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::add_perspective
+#' @inherit emuR::add_perspective return references description details sections seealso
+#' @seealso [emuR::add_perspective]
+add_perspective = emuR::add_perspective
+
+#' Remove a  perspective from an emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::remove_perspective
+#' @inherit emuR::remove_perspective return references description details sections seealso
+#' @seealso [emuR::remove_perspective]
+remove_perspective = emuR::remove_perspective
+
+#' List which perspectives are defined for an emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::list_perspectives
+#' @inherit emuR::list_perspectives return references description details sections seealso
+#' @seealso [emuR::list_perspectives]
+list_perspectives = emuR::list_perspectives
+
+#' Set the order of displayed levels for a perspective (from emuR)
+#' @export
+#' @inheritParams emuR::set_levelCanvasesOrder
+#' @inherit emuR::set_levelCanvasesOrder return references description details sections seealso
+#' @seealso [emuR::set_levelCanvasesOrder]
+set_levelCanvasesOrder = emuR::set_levelCanvasesOrder
+
+#' Get the level order of a perspective (from emuR)
+#' @export
+#' @inheritParams emuR::get_levelCanvasesOrder
+#' @inherit emuR::get_levelCanvasesOrder return references description details sections seealso
+#' @seealso [emuR::get_levelCanvasesOrder]
+get_levelCanvasesOrder = emuR::get_levelCanvasesOrder
+
+#' Set which signals are displayed in a perspective, and in which order (from emuR)
+#' @export
+#' @inheritParams emuR::set_signalCanvasesOrder
+#' @inherit emuR::set_signalCanvasesOrder return references description details sections seealso
+#' @seealso [emuR::set_signalCanvasesOrder]
+set_signalCanvasesOrder = emuR::set_signalCanvasesOrder
+
+#' Get which signals are displayed in a perspective, and in which order (from emuR)
+#' @export
+#' @inheritParams emuR::get_signalCanvasesOrder
+#' @inherit emuR::get_signalCanvasesOrder return references description details sections seealso
+#' @seealso [emuR::get_signalCanvasesOrder]
+get_signalCanvasesOrder = emuR::get_signalCanvasesOrder
+
+
+#' Create links between items (from emuR)
+#' @export
+#' @inheritParams emuR::create_links
+#' @inherit emuR::create_links return references description details sections seealso
+#' @seealso [emuR::create_links]
+create_links = emuR::create_links
+
+#' Duplicate a level (from emuR)
+#' @export
+#' @inheritParams emuR::duplicate_level
+#' @inherit emuR::duplicate_level return references description details sections seealso
+#' @seealso [emuR::duplicate_level]
+duplicate_level = emuR::duplicate_level
+
+
+#' Delete items programmatically (from emuR)
+#' @export
+#' @inheritParams emuR::delete_itemsInLevel
+#' @inherit emuR::delete_itemsInLevel return references description details sections seealso
+#' @seealso [emuR::delete_itemsInLevel]
+delete_itemsInLevel = emuR::delete_itemsInLevel
+
+
+
+#' Query an emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::query
+#' @inherit emuR::query return references description details sections seealso
+#' @seealso [emuR::query]
+query <- emuR::query
+
+
+# query <- function(...){
+#   out <- emuR::query(...) %>%
+#     tibble::rownames_to_column(var = "sl_rowIdx") %>%
+#     dplyr::mutate(sl_rowIdx=as.integer(sl_rowIdx))
+#   return(out)
+# }
+
+#' Requery the hierarchical context of a segment list (from emuR)
+#' @export
+#' @inheritParams emuR::requery_hier
+#' @inherit emuR::requery_hier return references description details sections seealso
+#' @seealso [emuR::requery_hier]
+requery_hier = emuR::requery_hier
+
+#' Requery the sequential context of a segment list (from emuR)
+#' @export
+#' @inheritParams emuR::requery_seq
+#' @inherit emuR::requery_seq return references description details sections seealso
+#' @seealso [emuR::requery_seq]
+requery_seq = emuR::requery_seq
+
+#' Get track data beloning to a segment list (from emuR)
+#' @export
+#' @inheritParams emuR::get_trackdata
+#' @inherit emuR::get_trackdata return references description details sections seealso
+#' @seealso [emuR::get_trackdata]
+get_trackdata = emuR::get_trackdata
+
+
+#' Re-exported function from emuR list the files of an emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::list_files
+#' @inherit emuR::list_files return references description details sections seealso
+#' @seealso [emuR::list_files]
+list_files = emuR::list_files
+
+#' Re-exported function from emuR list sessions in an emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::list_sessions
+#' @inherit emuR::list_sessions return references description details sections seealso
+#' @seealso [emuR::list_sessions]
+list_sessions = emuR::list_sessions
+
+#' List sessions in an emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::list_bundles
+#' @inherit emuR::list_bundles return references description details sections seealso
+#' @seealso [emuR::list_bundles]
+list_bundles = emuR::list_bundles
+
+#' Load an emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::load_emuDB
+#' @inherit emuR::load_emuDB return references description details sections seealso
+#' @seealso [emuR::load_emuDB]
+load_emuDB = emuR::load_emuDB
+
+
+#' Rename an emuDB
+#' @export
+#' @inheritParams emuR::rename_emuDB
+#' @inherit emuR::rename_emuDB return references description details sections seealso
+#' @seealso [emuR::rename_emuDB]
+rename_emuDB = emuR::rename_emuDB
+
+#' Serve EMU database to EMU-webApp (from emuR)
+#' @export
+#' @inheritParams emuR::serve
+#' @inherit emuR::serve return references description details sections seealso
+#' @seealso [emuR::serve]
+load_emuDB = emuR::serve
+
+#' List sample rates of media and annotation (_annot.json) files (from emuR)
+#' @export
+#' @inheritParams emuR::list_sampleRates
+#' @inherit emuR::list_sampleRates return references description details sections seealso
+#' @seealso [emuR::list_sampleRates]
+list_sampleRates = emuR::list_sampleRates
+
+#' Read a bundleList (from emuR)
+#' @export
+#' @inheritParams emuR::read_bundleList
+#' @inherit emuR::read_bundleList return references description details sections seealso
+#' @seealso [emuR::read_bundleList]
+read_bundleList = emuR::read_bundleList
+
+#' Rename bundles in emuDB (from emuR)
+#' @export
+#' @inheritParams emuR::rename_bundles
+#' @inherit emuR::rename_bundles return references description details sections seealso
+#' @seealso [emuR::rename_bundles]
+rename_bundles = emuR::rename_bundles
+
+#' Replace item labels (from emuR)
+#' @export
+#' @inheritParams emuR::replace_itemLabels
+#' @inherit emuR::replace_itemLabels return references description details sections seealso
+#' @seealso [emuR::replace_itemLabels]
+replace_itemLabels = emuR::replace_itemLabels
+
