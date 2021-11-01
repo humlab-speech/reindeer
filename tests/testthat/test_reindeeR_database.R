3451846cf74a917febaeee4c4f7b3bea39e43a3f
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-11-01 16:36:26 +0100

    Test code for EGG and fiel import.

---
+++ tests/testthat/test_reindeeR_database.R
@@ -0,0 +1,64 @@
+
+require(testthat)
+require(reindeer)
+
+test_that("Importation of speech signal files works",{
+  reindeer:::unlink_emuRDemoDir()
+  reindeer:::create_ae_db(verbose = FALSE) -> emuDBhandle
+  prolonged <- "../signalfiles/prolonged_a/"
+  #prolonged <- "tests/signalfiles/prolonged_a/"
+  expect_true(dir.exists(prolonged))
+
+  reindeer::import_recording(emuDBhandle,dir=prolonged,targetSessionName = "prolonged",verbose = FALSE)
+
+  expect_error(reindeer::import_recording(emuDBhandle,dir=prolonged,targetSessionName = "prolongedError",verbose = FALSE,speech.channel = 3))
+
+  pw <- list_files(emuDBhandle,sessionPattern = "prolonged",fileExtension = "wav")
+
+  expect_true(nrow(pw) == 3)
+
+  expect_true(nrow(list_sessions(emuDBhandle)) == 3  )
+
+  expect_true(nrow(list_bundles(emuDBhandle,session = "prolonged")) == 3 )
+
+  signalfile <- wrassp::read.AsspDataObj(pw$absolute_file_path[[1]])
+
+  expect_true(ncol(signalfile$audio) == 1)
+
+})
+
+
+test_that("Importation of EGG and speech signal files works",{
+  reindeer:::unlink_emuRDemoDir()
+  reindeer:::create_ae_db(verbose = FALSE) -> emuDBhandle
+  egg <- "../signalfiles/EGG/Session 1/"
+  #egg <- "tests/signalfiles/EGG/Session 1/"
+  expect_true(dir.exists(egg))
+
+  reindeer::import_recording(emuDBhandle,dir=egg,targetSessionName="EGG", verbose = FALSE,egg.channel = 2)
+
+  pw <- list_files(emuDBhandle,sessionPattern = "EGG",fileExtension = "wav")
+
+  expect_true(nrow(pw) == 1)
+
+  expect_true(nrow(list_sessions(emuDBhandle)) == 2 )
+
+  expect_true(nrow(list_bundles(emuDBhandle,session = "EGG")) == 1 )
+
+  signalfile <- wrassp::read.AsspDataObj(pw$absolute_file_path[[1]])
+
+  expect_true(ncol(signalfile$audio) == 1)
+
+  pe <- list_files(emuDBhandle,sessionPattern = "EGG",fileExtension = "egg")
+
+  expect_true(nrow(pe) == 1)
+
+  expect_true(nrow(list_bundles(emuDBhandle,session = "EGG")) == 1 )
+
+  eggfile <- wrassp::read.AsspDataObj(pe$absolute_file_path[[1]])
+
+  expect_true(ncol(eggfile$audio) == 1)
+
+
+})
+
