9dd16872e6434860a3315ef45101fc3af9c61be8
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-11-15 08:25:31 +0100

    Added function to create a more extensive metadata testing case with 11 session

--- R/emuR_develoment_utils.R
+++ R/emuR_develoment_utils.R
@@ -24,6 +24,27 @@ unlink_emuRDemoDir <- function(){
 }
 
 
+
+create_extended_ae_db <- function(verbose=FALSE){
+  demodir <- file.path(tempdir(),"emuR_demoData")
+
+  if(!dir.exists(demodir)){
+    emuR::create_emuRdemoData()
+  }
+  emuDBhandle <- emuR::load_emuDB(file.path(demodir,"ae_emuDB"),verbose=verbose)
+
+  dir.create(file.path(emuDBhandle$basePath,"temp"))
+  for(i in 1:10){
+    file.copy(file.path(emuDBhandle$basePath,"0000_ses"),file.path(emuDBhandle$basePath,"temp"),recursive = TRUE)
+    newName <- paste0(i,i,i,i,"_ses")
+    file.rename(file.path(emuDBhandle$basePath,"temp","0000_ses"),file.path(emuDBhandle$basePath,"temp",newName))
+    file.copy(file.path(emuDBhandle$basePath,"temp",newName),emuDBhandle$basePath,recursive = TRUE)
+    unlink(file.path(emuDBhandle$basePath,"temp",newName),recursive = TRUE)
+  }
+  unlink(file.path(emuDBhandle$basePath,"temp"))
+  return(emuDBhandle)
+}
+
 make_dummy_metafiles <- function(db,session=TRUE){
   outMetaFiles <- emuR::list_files(db,"wav") %>%
     dplyr::select(absolute_file_path) %>%
@@ -41,3 +62,28 @@ make_dummy_metafiles <- function(db,session=TRUE){
   }
 
 }
+
+add_dummy_metadata <- function(emuDBhandle){
+
+  metagrid <- expand.grid(Age=seq(1,76,3),Gender=c("Male","Female",NA)) %>% dplyr::arrange(Age) %>% dplyr::slice(-1)
+
+  bundles <- list_bundles(emuDBhandle)
+  for(i in 1:nrow(bundles)){
+      add_metadata(emuDBhandle,list(Age=metagrid[i,"Age"],Gender=metagrid[i,"Gender"]),session=bundles[i,"session"],bundle=bundles[i,"name"])
+  }
+
+  for(i in seq(1,nrow(bundles),1)){
+    if( i %% 13 == 0){
+      add_metadata(emuDBhandle,list(Gender=NULL),session=bundles[i,"session"],bundle=bundles[i,"name"])
+    }
+    if( i %%  31  == 0){
+      add_metadata(emuDBhandle,list(Age=NULL),session=bundles[i,"session"],bundle=bundles[i,"name"])
+    }
+
+  }
+}
+
+### INTERACTIVE TESTING
+# unlink_emuRDemoDir()
+# create_extended_ae_db() -> emuDBhandle
+# add_dummy_metadata(emuDBhandle)
