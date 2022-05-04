249727371a1c4a9a5c14a90f85e44af718fcf433
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2022-05-04 09:20:21 +0200

    New version of extract_samples
    * you can now append the values of named metadata fields.
    * you can also specify the output file name field separator.

--- R/reindeeR_segmentlist.R
+++ R/reindeeR_segmentlist.R
@@ -2,17 +2,36 @@
 
 #' Extract part of a mediafiles based on a segment list
 #'
-#' This function enables the user to extract parts of speech recordings based on a segment list (resulting from a call of [query]).
-#' The extracted parts will be named by the sample it was cut from, the first sample, last sample extracted, as well as the sample rate of the original media file. The extracted signal file will be placed in sub-folders according session and / or bundle if the user specifies it. If `create.session.subdir=FALSE,create.bundle.subdir=FALSE`, all extracted signal portions will be placed together in the output directory.
+#' This function enables the user to extract parts of speech recordings based on
+#' a segment list (resulting from a call of [query]). The extracted parts will
+#' be named by the segment label, the first sample, last sample extracted, as
+#' well as the sample rate of the original media file. The user can also append
+#' metadata fields to the file file name following the segment label. The
+#' extracted signal file will be placed in sub-folders according session and /
+#' or bundle if the user specifies it. If
+#' `create.session.subdir=FALSE,create.bundle.subdir=FALSE`, all extracted
+#' signal portions will be placed together in the output directory.
+#'
+#'
 #'
 #'
 #'
 #' @param emuDBhandle The database handle.
-#' @param seglist A segment list (resulting from a call of [query]) which should be used as a cut list.
-#' @param output.directory The directory where all extracted parts of the signal, and sub-folders if required, will be placed.
-#' @param include.labels Boolean; Include the label of a segment in the output file name?
-#' @param create.session.subdir Boolean; Should bundles belonging to different sessions be kept separate?
-#' @param create.bundle.subdir Boolean; Should signal files belonging to different bundles be kept separate?
+#' @param seglist A segment list (resulting from a call of [query]) which should
+#'   be used as a cut list.
+#' @param output.directory The directory where all extracted parts of the
+#'   signal, and sub-folders if required, will be placed.
+#' @param include.labels Boolean; Include the label of a segment in the output
+#'   file name?
+#' @param include.metadata.fields Names of metadata fields whos values should be
+#'   included in the file name. If a boolean (TRUE) all metadata fields will be
+#'   encoded in the name of the output file.
+#' @param create.session.subdir Boolean; Should bundles belonging to different
+#'   sessions be kept separate?
+#' @param create.bundle.subdir Boolean; Should signal files belonging to
+#'   different bundles be kept separate?
+#' @param field.separator The field separator string to use when constructing
+#'   the output file name.
 #'
 #' @export
 #'
@@ -26,13 +45,36 @@
 #' extract_samples(emuDBhandle,psl,output.directory = output.directory,create.session.subdir=TRUE,create.bundle.subdir=TRUE)
 #' print(list.files(path=output.directory))
 #'
-extract_samples <- function(emuDBhandle,seglist, output.directory,include.labels=FALSE,create.session.subdir=TRUE,create.bundle.subdir=TRUE){
+extract_samples <- function(emuDBhandle,seglist, output.directory,include.labels=FALSE,include.metadata.fields=FALSE,create.session.subdir=TRUE,create.bundle.subdir=TRUE,field.separator="_"){
   dbConfig <- emuR:::load_DBconfig(emuDBhandle)
   mediafileExtension <- dbConfig$mediafileExtension
   seglist$absolute_file_path <- file.path(emuDBhandle$basePath,
                                           paste0(seglist$session,emuR:::session.suffix),
                                           paste0(seglist$bundle,emuR:::bundle.dir.suffix),
                                           paste(seglist$bundle,mediafileExtension,sep="."))
+
+
+  if(  length(include.metadata.fields) > 1 || (is.logical(include.metadata.fields) & include.metadata.fields) ){
+    md <- get_metadata(emuDBhandle = emuDBhandle)
+
+    if(is.logical(include.metadata.fields) && include.metadata.fields){
+      include.metadata.fields <- setdiff(names(md),c("session","bundle"))
+    }else{
+      if(length(include.metadata.fields) > 0 ){
+        if(! all(include.metadata.fields %in% names(md))){
+          stop("Please make sure that all fields named in 'include.metadata.fields' are metadata fields in the database.")
+        }
+      }
+    }
+
+
+    mdsel <- cbind(md[,c("session","bundle")], md[,include.metadata.fields])
+    seglist <- seglist %>%
+      dplyr::left_join(mdsel,by=c("session","bundle")) %>%
+      tidyr::unite(labels, all_of(c("labels",include.metadata.fields)),sep=field.separator)
+
+  }
+
   for(r in 1:nrow(seglist)){
     out.dir <- ifelse(create.session.subdir,
                   file.path(output.directory, seglist[[r,"session"]]),
@@ -49,15 +91,17 @@ extract_samples <- function(emuDBhandle,seglist, output.directory,include.labels
                                         samples = TRUE)
 
     labelPart <- ifelse(include.labels,
-                        paste0("_",
+                        paste0(field.separator,
                                stringr::str_trim(seglist[[r,"labels"]],side="both"),
-                               "_"),
-                        "_")
+                               field.separator),
+                        field.separator)
+
+
 
     out.file <- file.path(out.dir,
                           paste0(seglist[[r,"bundle"]],
                                  labelPart,
-                                 seglist[[r,"sample_start"]],"_",seglist[[r,"sample_end"]],"_",seglist[[r,"sample_rate"]],
+                                 seglist[[r,"sample_start"]],field.separator,seglist[[r,"sample_end"]],field.separator,seglist[[r,"sample_rate"]],
                                  ".",mediafileExtension))
     wrassp::write.AsspDataObj(file = out.file, dobj = currSSF)
     rm(currSSF)
@@ -66,11 +110,14 @@ extract_samples <- function(emuDBhandle,seglist, output.directory,include.labels
 
 
 ### For interactive testing
-# library(reindeer)
-# reindeer:::unlink_emuRDemoDir()
-# reindeer:::create_ae_db() -> emuDBhandle
-# query(emuDBhandle,"Phonetic = p") -> psl
-# output.directory <- file.path(tempdir(),"reindeeR_extract")
-#  extract_samples(emuDBhandle,psl,output.directory = output.directory,create.session.subdir=TRUE,create.bundle.subdir=TRUE,include.labels = TRUE)
-# #
+#  library(reindeer)
+#  reindeer:::unlink_emuRDemoDir()
+#  reindeer:::create_ae_db() -> emuDBhandle
+#  reindeer:::add_dummy_metadata(emuDBhandle)
+#  query(emuDBhandle,"Phonetic = p") -> psl
+#  output.directory <- file.path(tempdir(),"reindeeR_extract")
+#  unlink(output.directory,recursive = TRUE)
+#  extract_samples(emuDBhandle,psl,output.directory = output.directory,create.session.subdir=FALSE,create.bundle.subdir=TRUE,include.labels = TRUE,include.metadata.fields = c("Gender","Age"))
+#  extract_samples(emuDBhandle,psl,output.directory = output.directory,create.session.subdir=FALSE,create.bundle.subdir=TRUE,include.labels = TRUE,include.metadata.fields =TRUE,field.separator="..")
+#
 # print(list.files(path=output.directory,recursive=TRUE))
