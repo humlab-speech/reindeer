34ea0cebc6774213fa6e64e693e551f8030e0f7b
Fredrik Nylén <fredrik.nylen@umu.se>
2025-11-14 14:50:15 +0100

    Version 0.1.1
    *Update of the package description
    * Reduced dependencies - sqldf, emuR, and glue are not needed
    * Deleted extract_samples as that should be re-implmented in the new package OOP structure.

--- R/reindeer_demodata.R
+++ R/reindeer_demodata.R
@@ -1,6 +1,6 @@
 
 
-emuR_ae <- function(verbose=FALSE){
+emu_ae <- function(verbose=FALSE){
   demodir <- file.path(tempdir(),"emuR_demoData")
 
   if(!dir.exists(demodir)){
@@ -9,3 +9,14 @@ emuR_ae <- function(verbose=FALSE){
   db <- reindeer::corpus(file.path(demodir,"ae_emuDB"),verbose=verbose)
   return(db)
 }
+
+ae <- function(verbose=FALSE){
+  demodir <- file.path(tempdir(),"emuR_demoData")
+
+  if(!dir.exists(demodir)){
+    utils::untar(system.file("extdata","aeflac.tar.xz",package="reindeer"),exdir=demodir)
+  }
+  db <- reindeer::corpus(file.path(demodir,"ae_emuDB"),verbose=verbose)
+  return(db)
+}
+
